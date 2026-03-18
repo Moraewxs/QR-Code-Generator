7909873d4b09b7d0b0e0a5394ee6ce9c05e2b767
Renato <sboy63622@gmail.com>
2026-03-18 19:48:43 -0300

    Javascript finalizado, funções adicionadas e conexão de api feita!

--- gerador.js
+++ gerador.js
@@ -1,1 +1,39 @@
-const x = true
+const container = document.querySelector(".container")
+const qrBtn = document.querySelector("#qr-header button")
+const qrInput = document.querySelector("#qr-header input")
+const qrImg = document.querySelector("#qr-code img")
+
+// Eventos 
+function generateQr() {
+    const qrInputValue = qrInput.value
+
+    console.log(qrInputValue)
+
+    if(!qrInputValue) return
+
+    qrBtn.innerText = "Gerando codigo..."
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrInputValue}`
+
+
+    qrImg.addEventListener("load", () => {
+        container.classList.add("active")
+        qrBtn.innerText = "Codigo QR gerado!"
+    })
+}
+qrBtn.addEventListener("click", () => {
+    generateQr()
+})
+
+qrInput.addEventListener("keydown", (e) => {
+    if(e.code === "Enter"){
+        generateQr()
+    }
+})
+
+qrInput.addEventListener("keyup", () => {
+
+    if(!qrInput.value) {
+        container.classList.remove("active")
+        qrBtn.innerText = "Gerar QR Code!"
+    }
+})
